7c515194b9229a20d2f66313e8d5e5cf19fca54d
Jdentity <identityjang@gmail.com>
2022-11-29 18:25:43 +0900

    :lipstick: Add InputForm component
    add InputForm component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import TopBar from './components/TopBar';
 import Layout from './common/Layout';
 import Container from './common/Container';
+import InputForm from './components/InputForm';
 
 function App() {
   return (
     <Layout>
       <Container>
         <TopBar />
+        <InputForm />
       </Container>
     </Layout>
   );
