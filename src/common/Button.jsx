c13f6ebde95d970b21d04d5c4bb59f5c70c563f8
Jdentity <identityjang@gmail.com>
2022-11-29 18:22:54 +0900

    :lipstick: Add Button component
    add button component

---
+++ src/common/Button.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledBtn = styled.button`
+  display: inline-block;
+  width: auto;
+  height: 45px;
+  padding: 10px 20px;
+  border-radius: 6px;
+  border-width: 2px;
+  border-style: solid;
+  font-size: 16px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+
+  cursor: pointer;
+
+  color: #fff;
+  background-color: ${(props) => props.color || '#2196f3'};
+  border-color: ${(props) => props.color || '#2196f3'};
+
+  box-shadow: 0 5px 5px -5px rgba($c, 0.15), 0 10px 10px -5px rgba($c, 0.15),
+    0 15px 15px -5px rgba($c, 0.15), 0 20px 20px -5px rgba($c, 0.15);
+
+  transition: 0.25s ease;
+
+  &:hover {
+    color: ${(props) => props.color || '#2196f3'};
+    border-color: ${(props) => props.color || '#2196f3'};
+    background: transparent;
+  }
+`;
+
+const Button = ({ color, children }) => {
+  return (
+    <StyledBtn color={color}>
+      <span>{children}</span>
+    </StyledBtn>
+  );
+};
+
+export default Button;
