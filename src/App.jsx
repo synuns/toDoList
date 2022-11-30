63cadf58414988ab793e512b0a52ab1a3c6ecaa9
Jdentity <identityjang@gmail.com>
2022-11-30 15:51:44 +0900

    :clown_face: Delete mock todos

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,6 @@ import { getDate } from './utils/date';
 
 const TODOS_KEY = 'todos';
 
-const mockToDo = [
-  {
-    id: 0,
-    title: '투두리스트 개발하기',
-    content: '리액트 기초를 다지기',
-    isDone: false,
-  },
-  {
-    id: 1,
-    title: '투두리스트 과제 제출',
-    content: '개발완료 후 배포하기',
-    isDone: false,
-  },
-  {
-    id: 2,
-    title: 'TIL 쓰기',
-    content: '개발블로그에 글 올리기',
-    isDone: false,
-  },
-  {
-    id: 3,
-    title: '팀 과제 작성하기',
-    content: '리액트 팀 과제 질문 작성',
-    isDone: false,
-  },
-  {
-    id: 4,
-    title: 'Virtual DOM 세션 듣기',
-    content: '듣기 싫다...',
-    isDone: true,
-  },
-];
-
 function App() {
   const [inputs, setInputs] = useState({
     title: '',
