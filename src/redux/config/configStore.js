1abbb826c4620b0af9d3a304884c461459077f44
Jdentity <identityjang@gmail.com>
2022-12-06 20:36:05 +0900

    ✨ save todos feature
    prevent refreshing todos

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,10 +2,20 @@ import { createStore } from "redux";
 import { combineReducers } from "redux";
 import toDoList from "../modules/toDoList";
 
+const TODOS_STORAGE_KEY = "toDos";
 
 const rootReducer = combineReducers({
   toDoList
 }); 
-const store = createStore(rootReducer); 
+
+const persistedState = localStorage.getItem(TODOS_STORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(TODOS_STORAGE_KEY))
+  : [];
+
+const store = createStore(rootReducer, persistedState); 
+
+store.subscribe(() => {
+  localStorage.setItem(TODOS_STORAGE_KEY, JSON.stringify(store.getState()))
+})
 
 export default store;
